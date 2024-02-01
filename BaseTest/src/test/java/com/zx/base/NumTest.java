78d0e2d941ea7939713db3944c924eab5f0cb9cc
小xun <1593437442@qq.com>
2024-02-01 10:06:40 +0800

    修改输出位置

--- BaseTest/src/test/java/com/zx/base/NumTest.java
+++ BaseTest/src/test/java/com/zx/base/NumTest.java
@@ -26,13 +26,13 @@ public class NumTest {
                 .forEachOrdered(System.out::println);
         //指数运算
         double result = Math.pow(2, 3);
+        System.out.println("计算2的3次方结果为:" + result);
         //& 按位与
         // | 按位或
         // ^ 按位异或
         // ~ 取反
         // << 左移
         // >> 右移
-        System.out.println("计算2的3次方结果为:" + result);
         //只有对应的两个二进位均为1时，结果位才为1，否则为0。参与运算的数以补码方式出现
         System.out.println("计算5、7的二进制按位与运算:" + (5 & 7));
         //只要对应的二个二进位有一个为1时，结果位就为 1。参与运算的两个数均以补码出现
