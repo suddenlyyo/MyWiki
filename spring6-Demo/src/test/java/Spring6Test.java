f51c3c1643f96ca963937f11d3683689a27ce029
小xun <1593437442@qq.com>
2024-02-19 20:22:57 +0800

    测试 spring6  使用xml配置bean

---
+++ spring6-Demo/src/test/java/Spring6Test.java
@@ -0,0 +1,27 @@
+import com.zx.model.Person;
+import lombok.Setter;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @program: MyWiki
+ * @description:
+ * @author: zhou  xun
+ * @create: 2024-02-19 18:49
+ */
+public class Spring6Test {
+
+//    构造器注入 适用于必需的、不可变的依赖，以及当你想在创建对象时立即设置其状态时。
+//    Setter 注入 适用于可选的、可变的依赖，以及当你需要在对象生命周期中的不同时间点注入依赖时。
+//    字段注入 是一种简洁的方式，但可能会导致代码的可读性和可维护性降低。它通常不推荐用于复杂的依赖注入场景。
+
+    @Test
+    public void createPerson() {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person) applicationContext.getBean("person");
+        System.out.println(person);
+    }
+
+}
